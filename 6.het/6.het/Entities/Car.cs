5a343df2a14ac74ecd81f170ee751e289aeebf62
Tivadar Tóth <tivadar.toth@stud.uni-corvinus.hu>
2022-11-04 12:45:12 +0100

    auto

--- 6.het/6.het/Entities/Car.cs
+++ 6.het/6.het/Entities/Car.cs
@@ -4,15 +4,15 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace _6.het.Entities
 {
-    public class Ball : Toy
+    internal class Car : Toy
     {
         protected override void DrawImage(Graphics g)
         {
-            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+            Image imageFile = Image.FromFile("car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
         }
     }
 }
